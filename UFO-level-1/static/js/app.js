c90f5bf67d2f3f70bf140730a098444a46a520d4
Laurie1990 <63030449+Laurie1990@users.noreply.github.com>
2020-10-04 01:32:43 +1000

    app update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -18,7 +18,21 @@ function runEnter() {
 //   console.log(tableData);
 // });
 
-};
+// // Step 5: Use d3 to update each cell's text with
+// // weather report values (weekday, date, high, low)
+data.forEach(function(tableData) {
+  console.log(tableData);
+  var row = tbody.append("tr");
+  Object.entries(tableData).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+
+
+});
 // // // Step 2:  Use d3 to append one table row `tr` for each weather report object
 // // // Don't worry about adding cells or text yet, just try appending the `tr` elements.
 // data.forEach(function(tableData) {
@@ -61,4 +75,4 @@ function runEnter() {
 //     var cell = row.append("td");
 //     cell.text(value);
 //   });
-// });
+};
