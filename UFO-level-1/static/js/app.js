2d276af64543beee59be92c2be8e1b0340003ccc
Laurie1990 <63030449+Laurie1990@users.noreply.github.com>
2020-10-05 00:00:53 +1100

    update to loop

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,28 +1,26 @@
 // from data.js
 var tableData = data;
 
-var tbody = d3.select("tbody");
-
 // Select the button
 var button = d3.select("#filter-btn");
 
-// Create event handlers 
-button.on("click", runEnter);
+// Select table
+var tbody = d3.select("tbody");
 
+// Select the input element and get the raw HTML node
 var inputElement = d3.select("#datetime");
-
+// Get the value property of the input element
 var inputValue = inputElement.property("value");
+
 
 // YOUR CODE HERE!
 // Step 1: Loop Through `data` and console.log each weather report object
 function runEnter() {
+
+  d3.event.preventDefault();
+
   console.log("Yeah buddy!");
 
-// Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-  
   console.log(inputValue)
 
 // // Step 5: Use d3 to update each cell's text with
@@ -38,8 +36,9 @@ data.forEach(function(tableData) {
     var cell = row.append("td");
     cell.text(value);
   });
-
-
 });
-
 };
+// Event handler for button click
+button.on("click", runEnter);
+//Update input on hitting Enter
+d3.select("form").on("submit", runEnter);
